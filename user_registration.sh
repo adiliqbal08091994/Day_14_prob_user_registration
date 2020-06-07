c68d86e94c1830ddfee76f59e9f2576cd89a7e66
adiliqbal08091994@gmail.com <adil.ds1994@gmail.com>
2020-06-07 17:09:05 +0530

    Added:first name validation

--- user_registration.sh
+++ user_registration.sh
@@ -2,3 +2,14 @@
 name_pat="^[A-Z]+[a-z]{2,}$"
 email_pat="^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.][a-zA-Z]{2,3}){1,2}$"
 phone_pat="^[+][0-9]{2}(\s)?[0-9]{10}$"
+while [[ true ]]
+do
+        read -p "enter your first name" f_name
+        if [[ $f_name =~ $name_pat ]]
+        then
+                #echo "valid"
+                break
+        else 
+                echo "enter correct first name"
+        fi
+done
